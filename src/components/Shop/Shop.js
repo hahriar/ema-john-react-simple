1e5f0bade48c9e4f7e84394dd71cae1c02cc6b14
hahriar <closebrocircle@gmail.com>
2022-05-15 15:48:51 +0600

    Long time push

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import'./Shop.css'
@@ -13,9 +14,24 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, [])
 
+    useEffect( () => {
+        if(products.length){
+            const savedCart = getStoredCart();
+            const storedCart = [];
+            for(const key in savedCart){
+                const addedProduct = products.find(product => product.
+                    key === key);
+                    storedCart.push(addedProduct);
+             }
+             setCart(storedCart);
+        }    
+    }, [products])
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        // Save To Local Storage
+
+        addToDb(product.key);
     }
 
     return (
